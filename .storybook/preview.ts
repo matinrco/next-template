854f9875f8b1a1eb98d1b89c676f894d5c4a2c9a
Matin Rezaei <matinrco@gmail.com>
2023-07-16 15:16:09 +0330

    Add locale btn to storybook toolbar

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { i18n } from "../next-i18next.config";
 
 const preview: Preview = {
     parameters: {
@@ -7,6 +8,22 @@ const preview: Preview = {
             matchers: {
                 color: /(background|color)$/i,
                 date: /Date$/,
+            },
+        },
+    },
+    globalTypes: {
+        locale: {
+            name: "Locale",
+            description: "Internationalization locale",
+            defaultValue: i18n.defaultLocale,
+            toolbar: {
+                icon: "globe",
+                items: i18n.locales.map((locale) => ({
+                    value: locale,
+                    title: locale,
+                })),
+                showName: true,
+                dynamicTitle: true,
             },
         },
     },
