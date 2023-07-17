7c2eefb454968612d5fcc762aca61d61dabc1967
Matin Rezaei <matinrco@gmail.com>
2023-07-17 19:42:11 +0330

    Integrate i18n into storybook

---
+++ .storybook/i18nDecorator.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import { appWithTranslation } from "next-i18next";
+import type { StoryFn, StoryContext } from "@storybook/react";
+import { i18n } from "../next-i18next.config";
+import { getDirection } from "../src/services/localeUtils";
+
+const resources: {
+    [key: string]: { [key: string]: { [key: string]: string } };
+} = ((ctx) => {
+    const keys = ctx.keys();
+    const values = keys.map(ctx);
+    const allFiles = keys.reduce((o, k, i) => {
+        o[k] = values[i];
+        return o;
+    }, {});
+
+    return i18n.locales.reduce((localeAccumulator, locale) => {
+        localeAccumulator[locale] = {
+            ...(() => {
+                const entriesInCurrentLocale = Object.entries(allFiles).filter(
+                    (entry) => entry[0].includes(`/${locale}/`),
+                );
+
+                const currentLocaleNamespaces = entriesInCurrentLocale.reduce(
+                    (currentLocaleAccumulator, entryInCurrentLocale) => {
+                        const currentNamespace = entryInCurrentLocale[0]
+                            .replace(/^.*[\\\/]/, "") // get file name from path string
+                            .replace(/\.[^/.]+$/, ""); // get namespace from file name
+                        currentLocaleAccumulator[currentNamespace] =
+                            entryInCurrentLocale[1];
+                        return currentLocaleAccumulator;
+                    },
+                    {},
+                );
+
+                return currentLocaleNamespaces;
+            })(),
+        };
+
+        return localeAccumulator;
+    }, {});
+})(require.context("../src/locales", true, /.*\.(json?)/));
+
+export default (Story: StoryFn, context: StoryContext) => {
+    const [locale, setLocale] = useState(i18n.defaultLocale);
+
+    const _nextI18Next = {
+        ns:
+            // extract all namespaces from resources
+            Object.entries(resources)
+                .map((entry) => entry[1])
+                .reduce<string[]>((acc, ns) => {
+                    const nsList = Object.keys(ns);
+
+                    nsList.forEach((nsItem) => {
+                        if (!acc.includes(nsItem)) {
+                            acc.push(nsItem);
+                        }
+                    });
+
+                    return acc;
+                }, []),
+        initialLocale: locale,
+        initialI18nStore: {
+            [locale]: {
+                ...resources[locale],
+            },
+        },
+        userConfig: {
+            resources,
+            i18n: {
+                locales: i18n.locales,
+                defaultLocale: locale,
+            },
+        },
+    };
+
+    useEffect(() => {
+        setLocale(context.globals.locale);
+        const direction = getDirection(context.globals.locale);
+        document.querySelector("html")?.setAttribute("dir", direction);
+        document
+            .querySelector("html")
+            ?.setAttribute("lang", context.globals.locale);
+    }, [context.globals.locale]);
+
+    const AppWithTranslation = appWithTranslation(Story);
+
+    return (
+        <AppWithTranslation
+            pageProps={{
+                _nextI18Next,
+            }}
+            // wtf is these
+            router={undefined as any}
+            Component={undefined as any}
+        />
+    );
+};
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from "@storybook/react";
 import { i18n } from "../next-i18next.config";
+import i18nDecorator from "./i18nDecorator";
+
+export const decorators = [i18nDecorator];
 
 const preview: Preview = {
     parameters: {
@@ -20,7 +23,7 @@ const preview: Preview = {
                 icon: "globe",
                 items: i18n.locales.map((locale) => ({
                     value: locale,
-                    title: locale,
+                    title: locale.toUpperCase(),
                 })),
                 showName: true,
                 dynamicTitle: true,
