35f5ac4b44f50bfeb992263f6956054d3007cf7e
Matin Rezaei <matinrco@gmail.com>
2023-07-24 01:36:36 +0330

    Fix color scheme flow in _app
    * add mantine ColorSchemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { GetServerSidePropsContext } from "next";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { getCookie, setCookie } from "cookies-next";
-import { MantineProvider, ColorScheme } from "@mantine/core";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
+import type { ColorScheme } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import { useColorScheme } from "@mantine/hooks";
 import { appWithTranslation } from "next-i18next";
@@ -15,49 +16,86 @@ import { getTheme } from "src/services/theme";
 import RouterTransition from "src/screens/shared/RouterTransition";
 import "src/services/global.scss";
 
+const COLOR_SCHEME = "color-scheme";
+const IS_COLOR_SCHEME_SET_EXPLICITLY = "is-color-scheme-set-explicitly";
+
 const CustomApp = ({
     Component,
-    colorScheme,
+    colorScheme: initialColorScheme,
     ...rest
-}: AppProps & { colorScheme: ColorScheme | undefined }): ReactElement => {
-    const preferredColorScheme = useColorScheme(colorScheme);
+}: AppProps & {
+    colorScheme?: ColorScheme;
+}): ReactElement => {
+    const preferredColorScheme = useColorScheme(initialColorScheme);
+    const [colorScheme, setColorScheme] = useState(
+        initialColorScheme === "dark" || initialColorScheme === "light"
+            ? initialColorScheme
+            : preferredColorScheme,
+    );
     const router = useRouter();
     const dir = getDirection(router?.locale || "");
     const { store, props } = wrapper.useWrappedStore(rest);
 
     useEffect(() => {
         document.querySelector("html")?.setAttribute("dir", dir);
-        setCookie("color-scheme", preferredColorScheme);
-    });
+    }, [dir]);
+
+    useEffect(() => {
+        setCookie(COLOR_SCHEME, colorScheme);
+    }, [colorScheme]);
+
+    useEffect(() => {
+        if (
+            preferredColorScheme !== colorScheme &&
+            !!!getCookie(IS_COLOR_SCHEME_SET_EXPLICITLY)
+        ) {
+            setColorScheme(preferredColorScheme);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [preferredColorScheme]);
 
     return (
         <ReactReduxProvider store={store}>
-            <MantineProvider
-                withGlobalStyles
-                withNormalizeCSS
-                theme={getTheme({
-                    dir,
-                    colorScheme: preferredColorScheme,
-                })}
-                emotionCache={(() => {
-                    switch (dir) {
-                        case "rtl":
-                            return rtlEmotionCache;
-                        case "ltr":
-                            return ltrEmotionCache;
+            <ColorSchemeProvider
+                colorScheme={colorScheme}
+                toggleColorScheme={(selectedColorScheme) => {
+                    if (selectedColorScheme) {
+                        setColorScheme(selectedColorScheme);
+                    } else {
+                        setColorScheme(
+                            colorScheme === "light" ? "dark" : "light",
+                        );
                     }
-                })()}
+                    setCookie(IS_COLOR_SCHEME_SET_EXPLICITLY, true);
+                }}
             >
-                <RouterTransition />
-                <Notifications position="top-center" />
-                <Component {...props.pageProps} />
-            </MantineProvider>
+                <MantineProvider
+                    withGlobalStyles
+                    withNormalizeCSS
+                    theme={getTheme({
+                        dir,
+                        colorScheme,
+                    })}
+                    emotionCache={(() => {
+                        switch (dir) {
+                            case "rtl":
+                                return rtlEmotionCache;
+                            case "ltr":
+                                return ltrEmotionCache;
+                        }
+                    })()}
+                >
+                    <RouterTransition />
+                    <Notifications position="top-center" />
+                    <Component {...props.pageProps} />
+                </MantineProvider>
+            </ColorSchemeProvider>
         </ReactReduxProvider>
     );
 };
 
 CustomApp.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-    colorScheme: getCookie("color-scheme", ctx),
+    colorScheme: getCookie(COLOR_SCHEME, ctx),
 });
 
 export default appWithTranslation(CustomApp);
