6788ce96330209957081f81b9331704b2f933e9c
Matin Rezaei <matinrco@gmail.com>
2023-07-24 01:30:15 +0330

    Implement color scheme switch in coordination with storybook color scheme
    * in storybook mantine decorator

--- .storybook/decorators/mantine.tsx
+++ .storybook/decorators/mantine.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import type { StoryFn, StoryContext } from "@storybook/react";
-import { useDarkMode } from "storybook-dark-mode";
-import { MantineProvider } from "@mantine/core";
+import { useChannel } from "@storybook/addons";
+import { useDarkMode, UPDATE_DARK_MODE_EVENT_NAME } from "storybook-dark-mode";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
 import { getTheme } from "../../src/services/theme";
 import { getDirection } from "../../src/services/localeUtils";
 import {
@@ -10,27 +11,34 @@ import {
 } from "../../src/services/emotionCache";
 
 export default (Story: StoryFn, context: StoryContext) => {
+    const emit = useChannel({});
+
     const dir = getDirection(context.globals.locale);
     const colorScheme = useDarkMode() ? "dark" : "light";
 
     return (
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={getTheme({
-                dir,
-                colorScheme,
-            })}
-            emotionCache={(() => {
-                switch (dir) {
-                    case "rtl":
-                        return rtlEmotionCache;
-                    case "ltr":
-                        return ltrEmotionCache;
-                }
-            })()}
+        <ColorSchemeProvider
+            colorScheme={colorScheme}
+            toggleColorScheme={() => emit(UPDATE_DARK_MODE_EVENT_NAME)}
         >
-            <Story />
-        </MantineProvider>
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={getTheme({
+                    dir,
+                    colorScheme,
+                })}
+                emotionCache={(() => {
+                    switch (dir) {
+                        case "rtl":
+                            return rtlEmotionCache;
+                        case "ltr":
+                            return ltrEmotionCache;
+                    }
+                })()}
+            >
+                <Story />
+            </MantineProvider>
+        </ColorSchemeProvider>
     );
 };
