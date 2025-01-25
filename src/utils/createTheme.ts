c4f6faa327b21ac7a27107e71b7e37e99779ed51
Matin Rezaei <matinrco@gmail.com>
2025-01-25 19:28:44 +0330

    Add sample `variantColorResolver` to theme

--- src/utils/createTheme.ts
+++ src/utils/createTheme.ts
@@ -1,4 +1,8 @@
-import { createTheme as mantineCreateTheme } from "@mantine/core";
+import {
+    defaultVariantColorsResolver,
+    createTheme as mantineCreateTheme,
+    parseThemeColor,
+} from "@mantine/core";
 import { vazirmatn } from "@/utils/fonts/vazirmatn";
 import { opensans } from "@/utils/fonts/opensans";
 
@@ -36,6 +40,36 @@ export const createTheme = (themeParams: ThemeParams) => {
             //     "#000000",
             //     "#000000",
             // ],
+        },
+        /**
+         * if you add a custom color to the theme,
+         * you may need to customize its behavior in different circumstances.
+         * use `variantColorResolver` to handle this.
+         */
+        variantColorResolver: (input) => {
+            const defaultResolvedColors = defaultVariantColorsResolver(input);
+            const parsedColor = parseThemeColor({
+                color: input.color || input.theme.primaryColor,
+                theme: input.theme,
+            });
+
+            /**
+             * just for demo only!!!
+             * delete or modify the below block according to your needs
+             */
+            if (parsedColor.color === "brand" && input.variant === "filled") {
+                if (
+                    typeof parsedColor.shade === "undefined" ||
+                    parsedColor.shade <= 6
+                ) {
+                    return {
+                        ...defaultResolvedColors,
+                        color: input.theme.colors.dark[7],
+                    };
+                }
+            }
+
+            return defaultResolvedColors;
         },
     });
 };
